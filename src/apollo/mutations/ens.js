f71b0585b2f16fd019ff7d2461150ef16620c903
Kasumi <kasumi.null@yandex.com>
2022-02-28 02:01:39 +0000

    chore: dev use bch.domains

--- src/apollo/mutations/ens.js
+++ src/apollo/mutations/ens.js
@@ -3,7 +3,7 @@ import { isENSReadyReactive, networkIdReactive } from '../reactiveVars'
 import { topLevelDomainReactive } from '../reactiveVars'
 
 const INFURA_ID =
-  window.location.host === 'app.ens.domains'
+  window.location.host === 'app.bch.domains'
     ? '90f210707d3c450f847659dc9a3436ea'
     : '58a380d3ecd545b2b5b3dad5d2b18bf0'
 
