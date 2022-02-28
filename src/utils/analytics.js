6bd8c112ce2917753d8056aa439a0c81dad7b2bf
Kasumi <kasumi.null@yandex.com>
2022-02-28 02:01:54 +0000

    chore: check production based on bch.domains

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -7,7 +7,7 @@ const TrackingID = {
 }
 
 function isProduction() {
-  return window.location.host === 'app.ens.domains'
+  return window.location.host === 'app.bch.domains'
 }
 
 function isDev() {
